88561c3df03e7fb0bcf3a016fb88fdc083e2615a
JordyLimaEng <jordylima51@gmail.com>
2018-03-29 11:03:27 -0300

    Update Counting_Sort.c

--- Counting_Sort/Counting_Sort.c
+++ Counting_Sort/Counting_Sort.c
@@ -28,7 +28,7 @@ void countingSort(int * Vetor, int Tam){
 
   int atual = 0;
   int max = Maior(Vetor, Tam);
-  int * Vetor_contador = calloc(max, sizeof(int)); // Zeros out the array
+  int * Vetor_contador = calloc(max, sizeof(int));
 
   for(atual = 0; atual < Tam; atual ++){
     Vetor_contador[Vetor[atual]]++;
@@ -51,20 +51,20 @@ void countingSort(int * Vetor, int Tam){
 
 int main(){
 
-  int test1[] = {2, 6, 4, 3, 2, 3, 4, 6, 3, 4, 3, 5, 2, 6};
+  int teste1[] = {2, 6, 4, 3, 2, 3, 4, 6, 3, 4, 3, 5, 2, 6};
   int Tam1 = 14;
 
-  countingSort(test1, Tam1);
+  countingSort(teste1, Tam1);
 
-  int test2[] = {5, 6, 7, 8, 5};
+  int teste2[] = {5, 6, 7, 8, 5};
   int Tam2 = 5;
 
-  countingSort(test2, Tam2);
+  countingSort(teste2, Tam2);
 
-  int test3[] = {8, 1, 2, 3, 3, 4};
+  int teste3[] = {8, 1, 2, 3, 3, 4};
   int Tam3 = 6;
 
-  countingSort(test3, Tam3);
+  countingSort(teste3, Tam3);
 
   return 0;
 }
